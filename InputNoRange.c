6d83d7b3185c16cdf96bdce093f0ba89fe765a3b
DHANUSHXENO <dhanushxenocrate@gmail.com>
2016-12-26 12:24:29 +0530

    Add files via upload

---
+++ InputNoRange.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,range;
+    printf("Enter a range\n");
+    scanf("%d",&range);
+    do
+    {
+        printf("Enter a num\n");
+        scanf("%d",&a);
+        if(a>range)
+            printf("Enter number in range\n");
+    }while(a>range);
+    printf("The number in range is %d",a);
+}
+
